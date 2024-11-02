d7b45ceb5a68a83a1cf224dae87f184a4bdc61dc
FMC-cat <jimmyfu901204@gmail.com>
2024-11-02 23:08:05 +0800

    Update

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleThemeButton = document.createElement("button");
-    toggleThemeButton.textContent = "切換深色/淺色模式";
-    document.body.prepend(toggleThemeButton);
-
-    toggleThemeButton.addEventListener("click", () => {
-        document.body.classList.toggle("dark-theme");
-    });
-});
